c766fa78d0259e990a6eac8ad154e56aa1e39b4b
devaspirant0510 <seungho020510@gmail.com>
2022-08-15 20:10:01 +0900

    feat : cpp socket client,server, node client

---
+++ socket/node_client/app.js
@@ -0,0 +1,10 @@
+const net = require("net");
+const client = net.createConnection({ port: 1234 }, () => {
+  // 'connect' listener.
+  console.log('connected to server!');
+  client.write('world!\r\n');
+});
+
+client.on('data',(data)=>{
+    console.log(data.toString('utf-8'));
+})
