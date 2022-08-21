3e798909f4a5dbc2a41bbed014765fd2df7c9838
devaspirant0510 <seungho020510@gmail.com>
2022-08-21 12:45:20 +0900

    feat : 서버,클라이언트 코드 변경

--- socket/SocketClient.cpp
+++ socket/SocketClient.cpp
@@ -1,27 +1,33 @@
 #include <stdio.h>
+#include <iostream>
 #include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <arpa/inet.h>
 #include <sys/socket.h>
-     
-int main(){
-       int sock = socket(AF_INET, SOCK_STREAM, 0);
- //    （   IP   ）    
- struct sockaddr_in serv_addr;
- memset(&serv_addr, 0, sizeof(serv_addr)); //      0  
- serv_addr.sin_family = AF_INET; //  IPv4  
- serv_addr.sin_addr.s_addr = inet_addr("127.0.0.1"); //   IP  
- serv_addr.sin_port = htons(1234); //  
- connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr));
- char buffer[40];
- while(1){
 
- read(sock, buffer, sizeof(buffer)-1);
- printf("Message form server: %s\n", buffer);
- }
- char msg[] = "Funck you Man";
- write(sock,msg,sizeof(msg)-1);
- close(sock);
- return 0;
+#define PORT 8002
+
+const int buffer_size = 4096;
+char buffer[buffer_size];
+     
+using namespace std;
+
+int main(){
+      int sock = socket(AF_INET, SOCK_STREAM, 0);
+      struct sockaddr_in serv_addr;
+      memset(&serv_addr, 0, sizeof(serv_addr)); //      0  
+      serv_addr.sin_family = AF_INET; //  IPv4  
+      serv_addr.sin_addr.s_addr = inet_addr("127.0.0.1"); //   IP  
+      serv_addr.sin_port = htons(PORT); //  
+      connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr));
+      recv(sock,buffer,sizeof(buffer)-1,0);
+      cout << "server : " << buffer << endl;
+      char msg[] = "Funck you Man";
+      while(1){
+            fgets(buffer,sizeof(buffer),stdin);
+            write(sock,buffer,sizeof(buffer)-1);      
+      }
+      close(sock);
+      return 0;
 }
--- socket/SocketServer.cpp
+++ socket/SocketServer.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <stdio.h>
@@ -7,32 +8,59 @@
 #include <arpa/inet.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
-int main(){
- int serv_sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
- //     IP、    
- struct sockaddr_in serv_addr;
- memset(&serv_addr, 0, sizeof(serv_addr));  //      0  
- serv_addr.sin_family = AF_INET; //  IPv4  
- serv_addr.sin_addr.s_addr = inet_addr("127.0.0.1"); //   IP  
-     
- serv_addr.sin_port = htons(1234); //  
-     
- bind(serv_sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr));
- listen(serv_sock, 20);
- struct sockaddr_in clnt_addr;
- socklen_t clnt_addr_size = sizeof(clnt_addr);
- int clnt_sock = accept(serv_sock, (struct sockaddr*)&clnt_addr, &clnt_addr_size);
 
-char str[] = "asdf";
-while(1){
- write(clnt_sock, str, sizeof(str));
-}
-//  char buffer[50];
-//  read(serv_sock,&buffer,sizeof(buffer-1));
-//  printf("client message : %s",buffer);
- close(clnt_sock);
- close(serv_sock);
- return 0;
-}
+#define PORT 8002
 
-    
+using namespace std;
+
+const int buffer_size = 4096;
+char buffer[buffer_size];
+bool isExit = false;
+
+int main()
+{
+    int serv_sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if (serv_sock < 0)
+    {
+        cout << "socket error " << endl;
+        return 0;
+    }
+    cout << "socket created !! " << endl;
+    //     IP、
+    struct sockaddr_in serv_addr;
+    serv_addr.sin_family = AF_INET;                     //  IPv4
+    serv_addr.sin_addr.s_addr = inet_addr("127.0.0.1"); //   IP
+    serv_addr.sin_port = htons(PORT);                   //
+
+    if(bind(serv_sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr))<0){
+        cout << "socket bind error " << endl;
+        return 0;
+    }
+    cout << "bind !!" << endl;
+    listen(serv_sock, 20);
+    cout << "listen !!" << endl;
+    struct sockaddr_in clnt_addr;
+    socklen_t clnt_addr_size = sizeof(clnt_addr);
+    int clnt_sock = accept(serv_sock, (struct sockaddr *)&clnt_addr, &clnt_addr_size);
+    if(clnt_sock<0){
+        cout << "client socket error " << endl;
+        return 0;
+    }
+    cout << "client socket accept !! "<< endl;
+    strcpy(buffer,"server connection");
+    send(clnt_sock,buffer,sizeof(buffer)-1,0);
+    memset(buffer,0,sizeof(buffer));
+    do{
+        memset(buffer,0,sizeof(buffer));
+        recv(clnt_sock,buffer,sizeof(buffer)-1,0);
+        if(strlen(buffer)!=0){
+            cout << "client : "<< buffer ;
+        }
+        if(strcmp(buffer,"exit\n")==0){
+            cout << "socket disconnect "<< endl;
+            close(serv_sock);
+            close(clnt_sock);
+        }
+    }while(!isExit);
+        
+}
